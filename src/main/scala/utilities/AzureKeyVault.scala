e78226df862c44ce207ec4cf4165a92c55d9dcd4
Jon Soloway <jon.soloway@hmcts.net>
2025-05-01 19:13:53 +0100

    Updating KV secret code

--- src/main/scala/utilities/AzureKeyVault.scala
+++ src/main/scala/utilities/AzureKeyVault.scala
@@ -5,9 +5,25 @@ import com.azure.security.keyvault.secrets.SecretClientBuilder
 
 object AzureKeyVault {
 
-  def getSecret(vaultName: String, secretName: String): String = {
+  /**
+   * Resolve the client secret by first checking the environment.
+   * If not found, fetch it from Azure Key Vault using the provided vault and secret name.
+   */
+  def loadClientSecret(vaultName: String, secretName: String): String = {
+    sys.env.get("CLIENT_SECRET") match {
+      case Some(secret) =>
+        println("CLIENT_SECRET loaded from environment")
+        secret
+      case None =>
+        println(s"CLIENT_SECRET not found in environment, fetching from Azure Key Vault: $vaultName / $secretName")
+        getSecretFromKeyVault(vaultName, secretName)
+    }
+  }
+
+  private def getSecretFromKeyVault(vaultName: String, secretName: String): String = {
+    val vaultUrl = s"https://$vaultName.vault.azure.net"
     val client = new SecretClientBuilder()
-      .vaultUrl(s"https://$vaultName.vault.azure.net")
+      .vaultUrl(vaultUrl)
       .credential(new DefaultAzureCredentialBuilder().build())
       .buildClient()
 
