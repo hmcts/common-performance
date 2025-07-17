5a0a65621f4287b7869b375ec6f1c354c766193a
jmcadam <mcadam88@gmail.com>
2025-07-17 17:22:06 +0100

    Update Headers.scala

--- src/main/scala/xui/Headers.scala
+++ src/main/scala/xui/Headers.scala
@@ -16,6 +16,7 @@ object Headers {
     "accept-encoding" -> "gzip, deflate, br",
     "accept-language" -> "en-GB,en;q=0.9",
     "content-type" -> "application/json",
+    "experimental" -> "true",
     "sec-fetch-dest" -> "empty",
     "sec-fetch-mode" -> "cors",
     "sec-fetch-site" -> "same-origin")
